584a797240cbd768f9cb24f63c23259d4cbc627f
rubick <shenpengfei@bilibili.com>
2025-04-08 21:08:34 +0800

    test-line

---
+++ examples/src/compute-shader/main.tsx
@@ -0,0 +1,156 @@
+import { createSignal } from 'solid-js'
+import { render } from 'solid-js/web'
+import {
+  Canvas,
+  createMaterial,
+  createOrbitControl,
+  createPlaneGeometry,
+  createUniformBufferBase,
+  Mesh,
+  PerspectiveCamera,
+  Vec3,
+  Vec3Like,
+  type CameraRef
+} from 'solid-webgpu'
+
+const formLineVertices = (options: {
+  pointA: Vec3Like
+  pointB: Vec3Like
+  lineWidth: number
+  segments: number
+  camera: CameraRef
+}) => {
+  const { pointA, pointB, lineWidth, segments, camera } = options
+  const vertices = []
+  const indices = []
+  let vertexCount = 0
+
+  const direction = Vec3.subtract(Vec3.create(), pointB, pointA)
+  direction.normalize()
+
+  const normal = Vec3.cross(Vec3.create(), direction, camera.up()).normalize()
+
+  vertices.push(
+    // position, tangent, normal, side, isEndpoint, endpointAngle
+    [pointA, direction, normal, -1.0, 0, 0.0],
+    [pointA, direction, normal, 1.0, 0, 0.0],
+    [pointB, direction, normal, -1.0, 0, 0.0],
+    [pointB, direction, normal, 1.0, 0, 0.0]
+  )
+  // main box
+  indices.push(0, 1, 2, 2, 1, 3)
+  vertexCount = 4
+
+  const startCenter = vertexCount
+  vertices.push([pointA, direction, normal, 0.0, 1, 0.0]) // 中心点
+  vertexCount++
+
+  for (let i = 0; i <= segments; i++) {
+    const angle = (Math.PI * i) / segments // 0 到 π
+    vertices.push([
+      pointA,
+      direction,
+      normal,
+      i === 0 ? -1.0 : i === segments ? 1.0 : Math.sin(angle),
+      1,
+      angle / Math.PI
+    ])
+
+    if (i > 0) {
+      indices.push(startCenter, vertexCount - 1, vertexCount)
+    }
+    vertexCount++
+  }
+
+  const endCenter = vertexCount
+  vertices.push([pointB, direction, normal, 0.0, 2, 0.0]) // 中心点
+  vertexCount++
+
+  for (let i = 0; i <= segments; i++) {
+    const angle = (Math.PI * i) / segments // 0 到 π
+    vertices.push([
+      pointB,
+      direction,
+      normal,
+      i === 0 ? -1.0 : i === segments ? 1.0 : Math.sin(angle),
+      2,
+      angle / Math.PI
+    ])
+
+    if (i > 0) {
+      indices.push(endCenter, vertexCount, vertexCount - 1)
+    }
+    vertexCount++
+  }
+
+  return { vertices, indices }
+}
+
+const App = () => {
+  const [camera, setCamera] = createSignal<CameraRef>()
+  const [canvas, setCanvas] = createSignal<HTMLCanvasElement>()
+
+  createOrbitControl(canvas, camera)
+
+  const lineData = new Float32Array([0, 0, 0, 0, 0, 1])
+
+  const planeGeo = createPlaneGeometry()
+
+  const { buffer: base, update: updateBase } = createUniformBufferBase()
+  const mat = createMaterial(
+    `
+struct VertexInput {
+    @location(0) POSITION: vec3<f32>,
+    @location(1) NORMAL: vec3<f32>,
+    @location(2) TANGENT: vec4<f32>,
+    @location(3) TEXCOORD_0: vec2<f32>
+};
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) uv: vec2<f32>,
+};
+
+struct BaseUniforms {
+    model: mat4x4<f32>,
+    view: mat4x4<f32>,
+    projection: mat4x4<f32>,
+    model_view: mat4x4<f32>,
+    normal_matrix: mat3x3<f32>,
+    camera_position: vec3<f32>
+};
+
+
+@group(0) @binding(0)
+var<uniform> uniforms: BaseUniforms;
+
+@vertex
+fn vs_main(input: VertexInput) -> VertexOutput {
+    var output: VertexOutput;
+    let world_position = (uniforms.model * vec4<f32>(input.POSITION, 1.0)).xyz;
+    let view_position = (uniforms.view * vec4<f32>(world_position, 1.0)).xyz;
+    output.clip_position = uniforms.projection * vec4<f32>(view_position, 1.0);
+    output.uv = input.TEXCOORD_0;
+    return output;
+}
+@fragment
+fn fs_main(input: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(input.uv, 0.5, 1.0);
+}
+
+    
+    `,
+    [base],
+    updateBase
+  )
+
+  return (
+    <Canvas camera={camera()} ref={setCanvas}>
+      <PerspectiveCamera label="main_camera" ref={setCamera} position={[0, 0, 5]} aspect={16 / 9} />
+
+      <Mesh geometry={planeGeo} material={mat()} />
+    </Canvas>
+  )
+}
+
+render(() => <App />, document.getElementById('app')!)
