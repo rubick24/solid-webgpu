03132e50ec5b17714027f0467e5fe9556bf537ee
rubick <shenpengfei@bilibili.com>
2025-04-15 14:30:19 +0800

    update

--- packages/solid-webgpu/src/utils.ts
+++ packages/solid-webgpu/src/utils.ts
@@ -17,10 +17,13 @@ export const clamp = (min: number, max: number, v: number) => {
   return Math.max(min, Math.min(max, v))
 }
 
-export const imageBitmapFromImageUrl = async (url: string) => {
+export const imageBitmapFromImageUrl = async (url: string, options?: ImageBitmapOptions) => {
   const response = await fetch(url)
   const blob = await response.blob()
-  return createImageBitmap(blob)
+  return createImageBitmap(blob, {
+    imageOrientation: 'flipY',
+    ...options
+  })
 }
 
 export const setBitOfValue = (val: number, offset: number, bit: boolean) => {
@@ -32,6 +35,6 @@ export const white1pxBase64 =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII='
 
 export const access = <T extends MaybeAccessor<any>>(v: T): MaybeAccessorValue<T> =>
-  typeof v === 'function' && !v.length ? v() : v
+  typeof v === 'function' && !v.length ? v() : (v as MaybeAccessorValue<T>)
 
 export const isAccessor = <T>(v: MaybeAccessor<T>): v is Accessor<T> => typeof v === 'function' && !v.length
